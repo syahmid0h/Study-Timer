f50272b3496e99cb7dc6e916c6652fee9cd709ab
syahmid0h <syahmisaif@gmail.com>
2024-08-27 01:50:46 +0800

    Set up URLS, Authentication, defined routes for Flask, some HTML stuff

--- website/__init__.py
+++ website/__init__.py
@@ -2,9 +2,12 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = '2M1P'
+    app.config['SECRET_KEY'] = '2M1P' #Our own private key for accessing
 
     from .views import views
     from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
     return app
--- website/auth.py
+++ website/auth.py
@@ -1,3 +1,16 @@
 from flask import Blueprint
 
 auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+
+def login():                #Login text, 3 of these is the defined routes
+    return "<p>Login</p>"   #<p></p> Is the paragraph HTML 
+
+@auth.route('/logout')      #Logout text, auth.route is for the url to go to the "Login", "Logout", "Signup" page when the end of the url has /logout, /login, /sign-up ykwim :)
+def logout():
+    return "<p>Logout</p>"
+
+@auth.route('/sign-up')     #Sign up text
+def sign_up():
+    return "<p>Sign Up</p>"
