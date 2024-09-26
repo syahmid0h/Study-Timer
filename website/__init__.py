b1e68c1590d7e19c7c4b17c53319b9a132e6b21e
syahmid0h <syahmisaif@gmail.com>
2024-09-26 17:59:55 +0800

    authnetication

--- website/__init__.py
+++ website/__init__.py
@@ -1,12 +1,21 @@
 from flask import Flask
 from .views import views
 from .auth import auth
+from .extensions import db, login_manager, bcrypt
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = '2M1P' #Our own private key for accessing
+    app.config['SECRET_KEY'] = '2M1P'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/database.db'
 
-    from website import views, auth
+    db.init_app(app)
+    login_manager.init_app(app)
+    bcrypt.init_app(app)
+
+    from .models import User
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
--- website/auth.py
+++ website/auth.py
@@ -1,19 +1,87 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required, current_user
+from flask_wtf import FlaskForm
+from .models import User
+from .extensions import db, bcrypt, login_manager
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/register')
+# This is our .form nihal -_- -syahmi
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=4, max=20)])
+    submit = SubmitField("Register")
+
+    def validate_username(self, username):
+        existing_user = User.query.filter_by(username=username.data).first()
+        if existing_user:
+            raise ValidationError('This username has already been taken. Please choose another one.')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=4, max=20)])
+    submit = SubmitField("Login")
+
+class ChangePasswordForm(FlaskForm):
+    current_password = PasswordField('Current Password', validators=[InputRequired()])
+    new_password = PasswordField('New Password', validators=[InputRequired(), Length(min=4, max=20)])
+    confirm_password = PasswordField('Confirm New Password', validators=[InputRequired(), Length(min=4, max=20)])
+    submit = SubmitField("Change Password")
+
+    def validate_confirm_password(self, confirm_password):
+        if confirm_password.data != self.new_password.data:
+            raise ValidationError('Passwords do not match.')
+   
+
+@auth.route('/register', methods=['GET','POST'])
 def register():
-    return render_template('register.html')
+    if current_user.is_authenticated:
+        return redirect(url_for('views.index'))
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Account successfully created! You can now log in.')
+        return redirect(url_for('auth.login'))
+    return render_template('register.html', form=form)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if current_user.is_authenticated:
+        return redirect(url_for('views.index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user)
+            return redirect(url_for('views.index'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', form=form)
 
-@auth.route('/account')
-def account():
-    return render_template('account.html')
+@auth.route("/account", methods=['GET', 'POST'])
+@login_required
+def change_password():
+    form = ChangePasswordForm()
+    if form.validate_on_submit():
+        if bcrypt.check_password_hash(current_user.password, form.current_password.data):
+            hashed_password = bcrypt.generate_password_hash(form.new_password.data).decode('utf-8')
+            current_user.password = hashed_password
+            db.session.commit()
+            flash('Password updated successfully!', 'success')
+            return redirect(url_for('views.index'))
+        else:
+            flash('Current password is incorrect', 'danger')
+    return render_template('account.html', form=form)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return render_template('logout.html')
+    logout_user()
+    flash('You have been logged out', 'info')
+    return redirect(url_for('auth.login'))
