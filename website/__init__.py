556cdd8a399f7652c92d1f54b740e0fab57a2fbe
syahmid0h <syahmisaif@gmail.com>
2024-09-26 21:50:39 +0800

    some tweaks i dont remember

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,20 @@
+import os
 from flask import Flask
 from .views import views
 from .auth import auth
 from .extensions import db, login_manager, bcrypt
+from flask_migrate import Migrate
+
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = '2M1P'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/database.db'
 
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    app.config['SECRET_KEY'] = '2M1P'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, '..', 'instance', 'database.db')
+
+    migrate.init_app(app, db)
     db.init_app(app)
     login_manager.init_app(app)
     bcrypt.init_app(app)
